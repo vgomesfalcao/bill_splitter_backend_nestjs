747dc0f7b5f9b5aaf498f21b56f5c4b454b0da56
Vinicius Gomes Falcão <vgomesfalcao@gmail.com>
2023-02-24 02:05:51 -0300

    Feat(auth): Acess token jwt generation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common'
 import { AuthController } from './auth.controller'
+import { JwtModule } from '@nestjs/jwt'
 import { AuthService } from './auth.service'
 import { UserModule } from 'src/user/user.module'
 import { LocalStrategy } from './strategies/local.strategy'
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '30d' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,28 @@
 import { Injectable } from '@nestjs/common'
 import { UserService } from 'src/user/user.service'
 import * as bcrypt from 'bcrypt'
+import { UserEntity } from 'src/user/entities/user.entity'
+import { UserPayload } from './models/UserPayload'
+import { JwtService } from '@nestjs/jwt'
+import { UserToken } from './models/UserToken'
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
-  login() {
-    throw new Error('Method not implemented.')
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  login(user: UserEntity): UserToken {
+    // Transforma o user em um JWT
+    const payload: UserPayload = {
+      sub: user.id,
+      email: user.email,
+      name: user.name,
+    }
+    const jwtToken = this.jwtService.sign(payload)
+    return {
+      access_token: jwtToken,
+    }
   }
 
   async validateUser(email: string, password: string) {
